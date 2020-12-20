945e714f6c6b94a11641ea1dc8076c56a5db3a1a
nikhilanand01 <nikhilf@fudinc.com>
2020-12-21 00:15:16 +0100

    adding screen detection for mobile and desktop versions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import CalcPage from './CalcPage.js';
 import DesktopVersion from './DesktopVersion.js';
+import MobileVersion from './MobileVersion.js';
+import {BrowserView, MobileView, isBrowser, isMobile} from "react-device-detect";
 
 
 function App() {
   return (
     <div className="App">
-      {/*<CalcPage/>*/}
-      <DesktopVersion/>
+      <BrowserView>
+        <DesktopVersion/>
+      </BrowserView>
+      <MobileView>
+        <MobileVersion/>
+      </MobileView>
     </div>
   );
 }
---
+++ src/MobileVersion.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import SmallText from './components/SmallText.js';
+import TitleText from './components/TitleText.js';
+
+class MobileVersion extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+      <div style={{textAlign: 'center'}}>
+        <TitleText text="The MOBILE Streaming Calculator" style={{color: '#111', fontSize: '28px', fontWeight: '700', justifyContent: 'center', textTransform: 'uppercase'}}/>
+        <SmallText text="COMING SOON"/>
+      </div>
+    )
+  }
+}
+
+export default MobileVersion;
