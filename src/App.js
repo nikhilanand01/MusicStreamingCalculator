23e6c1c38fab554b40b1d69a275b02af3dc572de
nikhilanand01 <nikhilf@fudinc.com>
2020-12-22 19:26:41 +0100

    more cleanup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import CalcPage from './CalcPage.js';
 import DesktopVersion from './DesktopVersion.js';
 import MobileVersion from './MobileVersion.js';
 import {BrowserView, MobileView} from "react-device-detect";
