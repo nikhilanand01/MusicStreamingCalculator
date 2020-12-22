56c4e605203efcd4f7fae33327a15e4c7b67026d
nikhilanand01 <nikhilf@fudinc.com>
2020-12-22 16:15:48 +0100

    start google analytics

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import CalcPage from './CalcPage.js';
 import DesktopVersion from './DesktopVersion.js';
 import MobileVersion from './MobileVersion.js';
 import {BrowserView, MobileView} from "react-device-detect";
+import ReactGA from 'react-ga';
+
+function initializeAnalytics(){
+  ReactGA.initialize("G-P56BLEJ9M3")
+  ReactGA.pageview('/')
+}
 
 
 function App() {
+  initializeAnalytics()
   return (
     <div className="App">
       <BrowserView>
